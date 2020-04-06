5e151f9691c741852da241a73e33677acadd1b4f
davhg96 <davidhg96@gmail.com>
2020-04-06 13:25:08 +0200

    Finished day 16 analysis. Comapre results with other datasets

--- source/day16Cluster.R
+++ source/day16Cluster.R
@@ -31,13 +31,77 @@ plot1 + plot2
 day16 <- subset(day16, subset = nFeature_RNA > 900 & nFeature_RNA < 4000 & percent.mt < 3)
 #subset cells with at least 500 genes and max 4000 and less than 3% mt contamination
 
+
 #Normalize the data
 #------------------
 
 day16 <- NormalizeData(day16,normalization.method = "LogNormalize", scale.factor = 10000)
 #Is logarithmic normalization good enough(?)
 
+
 #Feature selection
 #-----------------
+day16 <- FindVariableFeatures(day16, selection.method = "vst", nfeatures = 3000)
+#taking 3000 features out of 3100
 
-sag
+top20 <- head(VariableFeatures(day16),20)
+plot1 <- VariableFeaturePlot(day16)
+plot2 <- LabelPoints(plot = plot1, points = top20, repel = TRUE)
+plot1 + plot2
+
+
+#Scaling the data
+#----------------
+all.genes <- rownames(day16)
+day16 <- ScaleData(day16, features = all.genes)
+
+
+# Linear dimensional reduction
+#-----------------------------
+day16 <- RunPCA(day16, features = VariableFeatures(object = day16))
+
+VizDimLoadings(day16, dims = 1:3, reduction = "pca")
+
+DimPlot(day16, reduction = "pca")
+DimHeatmap(day16, dims = 1, cells = 1000, balanced = TRUE)
+DimHeatmap(day16, dims = 1:20, cells = 1000, balanced = TRUE)
+
+
+#Dimensionality
+#--------------
+
+day16 <- JackStraw(day16,num.replicate = 100)
+day16 <- ScoreJackStraw(day16, dims = 1:20)
+JackStrawPlot(day16, dims = 1:20)
+ElbowPlot(day16) #I'll take 9
+
+#Clustering
+#----------
+day16 <- FindNeighbors(day16,dims = 1:9)
+day16 <- FindClusters(day16, resolution = 0.1)
+
+#Non-linear reduction
+#--------------------
+day16 <- RunUMAP(day16, dims = 1:9)
+day16 <-RunTSNE(day16, dims = 1:9)
+DimPlot(day16, reduction = "umap")#With 0.1 resolution looks better
+DimPlot(day16, reduction = "tsne") #With 0.2 resolution looks better
+saveRDS(day16, file = "output/day16.rds")
+
+
+#Find clusters
+#-------------
+day16.markers <- FindAllMarkers(day16, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+#cluster
+day16.markers %>% group_by(cluster) %>% top_n(n = 2, wt = avg_logFC)
+#Sort the clusters
+
+#-------------
+head(day16.markers,10)
+VlnPlot(day16, features = c("TOP2A"))
+FeaturePlot(day16, features = "RPS27", reduction = "tsne")
+#--------------
+
+#HEatmap
+top10 <- day16.markers %>% group_by(cluster) %>% top_n(n = 10, wt = avg_logFC)
+DoHeatmap(day16, features = top10$gene) + NoLegend()
